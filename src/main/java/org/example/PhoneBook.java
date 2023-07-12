a6731694ea8aada32f7ecf890a0f1c435b4676c9
Vera Lisitsa <nvk1984@mail.ru>
2023-07-12 11:33:29 +0300

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,4 +1,8 @@
 package org.example;
 
 public class PhoneBook {
+
+    public int add(String name, String phoneNumber){
+       return 0;
+    }
 }
--- src/test/java/org/example/PhoneBookTests.java
+++ src/test/java/org/example/PhoneBookTests.java
@@ -1,4 +1,43 @@
 package org.example;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class PhoneBookTests {
+    PhoneBook phoneBook;
+
+    @BeforeEach
+    public void beforeEach() {
+        phoneBook = new PhoneBook();
+    }
+
+    @AfterEach
+    public void afterEach() {
+        phoneBook = null;
+    }
+
+    @Test
+    public void addTestNotNull() {
+        String name = "name", number = "123456";
+        int expected = 1;
+        int result = phoneBook.add(name, number);
+        Assertions.assertEquals(expected, result);
+    }
+    @Test
+    public void addTestNameNull(){
+        String name = null, number = "123456";
+        Class<IllegalArgumentException> expected = IllegalArgumentException.class;
+        Assertions.assertThrows(expected,() -> phoneBook.add(name ,number));
+    }
+
+    @Test
+    public void addTestNumberNull(){
+        String name = "name", number = null;
+        Class<IllegalArgumentException> expected = IllegalArgumentException.class;
+        Assertions.assertThrows(expected, () -> phoneBook.add(name,number));
+    }
+
+
 }
